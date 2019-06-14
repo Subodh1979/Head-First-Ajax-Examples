f1e47b833f20bbeaa0b172e523569ce845251a05
Venkatesh Thamburapu <37204830+thevenkateshthamburapu@users.noreply.github.com>
2019-06-14 23:02:12 +0530

    examples

--- Chapter-02 Designing Ajax Applications Thinking Ajaxian/scripts/validation.js
+++ Chapter-02 Designing Ajax Applications Thinking Ajaxian/scripts/validation.js
@@ -1,1 +1,54 @@
-window.onload = initPage;//alert("Browser Started");function initPage() {	//call checkUsername function  document.getElementById("username").onblur = checkUsername;  //disable submit button  document.getElementById("register").disabled = true;}function checkUsername() {	//display loading gif image  document.getElementById("username").className = "thinking";  //get the request object  request = createRequest();  if (request == null)    alert("Unable to create request");  else {//	  alert("Object/Request Created");	  //get the username    var theName = document.getElementById("username").value;    var username = escape(theName);    var url= "checkName.jsp?username=" + username;    //any changes call showUsernameStatus function    request.onreadystatechange = showUsernameStatus;    request.open("GET", url, true);    request.send(null);  }}function showUsernameStatus() {  if (request.readyState == 4) {    if (request.status == 200) {//    	alert("status : ok");    	var status=request.responseText;//    	alert("Response : "+request.responseText);      if (status.trim() == "okay") {    	  //display check mark        document.getElementById("username").className = "approved";        //enable submit button        document.getElementById("register").disabled = false;      } else {    	  //display cross mark        document.getElementById("username").className = "denied";        //focus the field        document.getElementById("username").focus();        //select the text        document.getElementById("username").select();        //disable submit button        document.getElementById("register").disabled = true;      }    }  }}
+window.onload = initPage;
+//alert("Browser Started");
+function initPage() {
+	//call checkUsername function
+  document.getElementById("username").onblur = checkUsername;
+  //disable submit button
+  document.getElementById("register").disabled = true;
+}
+
+function checkUsername() {
+	//display loading gif image
+  document.getElementById("username").className = "thinking";
+  //get the request object
+  request = createRequest();
+  if (request == null)
+    alert("Unable to create request");
+  else {
+//	  alert("Object/Request Created");
+	  //get the username
+    var theName = document.getElementById("username").value;
+    var username = escape(theName);
+    var url= "checkName.jsp?username=" + username;
+    //any changes call showUsernameStatus function
+    request.onreadystatechange = showUsernameStatus;
+    request.open("GET", url, true);
+    request.send(null);
+  }
+}
+
+function showUsernameStatus() {
+  if (request.readyState == 4) {
+    if (request.status == 200) {
+//    	alert("status : ok");
+    	var status=request.responseText;
+//    	alert("Response : "+request.responseText);
+      if (status.trim() == "okay") {
+    	  //display check mark
+        document.getElementById("username").className = "approved";
+        //enable submit button
+        document.getElementById("register").disabled = false;
+      } else {
+    	  //display cross mark
+        document.getElementById("username").className = "denied";
+        //focus the field
+        document.getElementById("username").focus();
+        //select the text
+        document.getElementById("username").select();
+        //disable submit button
+        document.getElementById("register").disabled = true;
+      }
+    }
+  }
+}
+ 
