6fd902e53a153e09b73aa3b624dafb72c35150fe
Venkatesh Thamburapu <37204830+thevenkateshthamburapu@users.noreply.github.com>
2019-06-14 23:55:37 +0530

    examples

--- Chapter-09 XML Requests and Responses More Than Words Can Say/scripts/thumbnails-normal.js
+++ Chapter-09 XML Requests and Responses More Than Words Can Say/scripts/thumbnails-normal.js
@@ -1,6 +1,11 @@
 window.onload = initPage;
 //alert("Browser Started");
-function initPage() {  // find the thumbnails on the page  thumbs = document.getElementById("thumbnailPane").getElementsByTagName("img");  // set the handler for each image  for (var i = 0; i < thumbs.length; i++) {
+function initPage() {
+  // find the thumbnails on the page
+  thumbs = document.getElementById("thumbnailPane").getElementsByTagName("img");
+
+  // set the handler for each image
+  for (var i = 0; i < thumbs.length; i++) {
     image = thumbs[i];
     
     // create the onclick function
@@ -12,12 +17,35 @@ function initPage() {  // find the thumbnails on the page  thumbs = document.g
       document.getElementById("itemDetail").src = detailURL;
 //      alert("Image changed");
       getDetails(this.title);
-    }  }}//get the item detailsfunction getDetails(itemName) {  //get the request object
-	request = createRequest();  if (request == null) {    alert("Unable to create request");    return;  }
-//  alert("Object/Request Created");  var url= "getDetails.jsp?ImageID=" + escape(itemName);  request.open("GET", url, true);
-  //any changes call displayDetails function   request.onreadystatechange = displayDetails;  request.send(null);}
-function displayDetails() {  if (request.readyState == 4) {    if (request.status == 200) {
+    }
+  }
+}
+//get the item details
+function getDetails(itemName) {
+  //get the request object
+	request = createRequest();
+  if (request == null) {
+    alert("Unable to create request");
+    return;
+  }
+//  alert("Object/Request Created");
+  var url= "getDetails.jsp?ImageID=" + escape(itemName);
+  request.open("GET", url, true);
+  //any changes call displayDetails function 
+  request.onreadystatechange = displayDetails;
+  request.send(null);
+}
+
+function displayDetails() {
+  if (request.readyState == 4) {
+    if (request.status == 200) {
 //    	alert("status : ok");
-    	//Get the element      detailDiv = document.getElementById("description");
+    	//Get the element
+      detailDiv = document.getElementById("description");
       //display the response text
-//      alert("Response : "+request.responseText);      detailDiv.innerHTML = request.responseText;    }  }}
+//      alert("Response : "+request.responseText);
+      detailDiv.innerHTML = request.responseText;
+    }
+  }
+}
+ 
